63dda654cb98c371dede19a934b3802baaf0dc67
kawabata <s1931115@s.konan-u.ac.jp>
2022-03-14 00:22:57 +0900

    implements number_of_attachment

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
   validate :validate_content_length
   validate :validate_byte_size
+  validate :validate_number_of_attachment
 
   MAX_CONTENT_LENGTH = 50
 
@@ -12,29 +13,43 @@ class Post < ApplicationRecord
   MAX_MEGA_BYTE_SIZE = 1
   MAX_CONTENT_ATTACHMENT_BYTE_SIZE = MAX_MEGA_BYTE_SIZE * ONE_KILO_BYTE * 1_000
 
+  MAX_NUMBER_OF_ATTACHMENT = 4
+
   private
 
-  def validate_content_length
-    length = content.to_plain_text.length
-    if length > MAX_CONTENT_LENGTH
-      errors.add(:content, :too_long, max_content_length: MAX_CONTENT_LENGTH, length: length)
-    end
-  end
-
-  def validate_byte_size
-    content.body.attachables.grep(ActiveStorage::Blob) do |attachable|
-      byte_size = attachable.byte_size
-
-      if attachable.byte_size > MAX_CONTENT_ATTACHMENT_BYTE_SIZE
-        errors.add(
-          :base,
-          :content_attachment_is_too_big,
-          max_content_attachment_byte_size: MAX_CONTENT_ATTACHMENT_BYTE_SIZE,
-          byte_size: byte_size,
-          max_mega_bite_size: MAX_MEGA_BYTE_SIZE
-        )
+    def validate_content_length
+      length = content.to_plain_text.length
+      if length > MAX_CONTENT_LENGTH
+        errors.add(:content, :too_long, max_content_length: MAX_CONTENT_LENGTH, length: length)
       end
     end
 
-  end
+    def validate_byte_size
+      content.body.attachables.grep(ActiveStorage::Blob) do |attachable|
+        byte_size = attachable.byte_size
+
+        if byte_size > MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+          errors.add(
+            :base,
+            :content_attachment_is_too_big,
+            max_content_attachment_byte_size: MAX_CONTENT_ATTACHMENT_BYTE_SIZE,
+            byte_size: byte_size,
+            max_mega_bite_size: MAX_MEGA_BYTE_SIZE
+          )
+        end
+      end
+    end
+
+    def validate_number_of_attachment
+      number_of_attachment = content.body.attachables.grep(ActiveStorage::Blob).count
+
+      if number_of_attachment > MAX_NUMBER_OF_ATTACHMENT
+        errors.add(
+          :base,
+          :content_attachment_number_is_too_many,
+          number_of_attachment: number_of_attachment,
+          max_number_of_attachment: MAX_NUMBER_OF_ATTACHMENT
+        )
+      end
+    end
 end
